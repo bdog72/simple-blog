90f7b954bafa6fa839a81c2c310b4cca7ecfa21b
Brian Bycynski <high5bri@hotmail.com>
2017-08-01 12:44:02 -0400

    added react router

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,16 +1,85 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
-export default class Postsnew extends Component {
+
+class Postsnew extends Component {
+  constructor (props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField (field) {
+    return (
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+        {field.meta.touched ? field.meta.error : ''}
+      </div>
+    )
+  }
+
+  onSubmit (values) {
+    console.log(values)
+  }
+
   render () {
+    const { handleSubmit } = this.props
+
     return (
       <div>
-        <div className='text-right'>
+        <div className='text-sm-right'>
           <Link className='btn btn-primary' to='/'>
-            Back to Home
+            Home
           </Link>
         </div>
-        <h2>New Posts</h2>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field
+            label='Title'
+            name='title'
+            component={this.renderField}
+          />
+          <Field
+            label='Categories'
+            name='categories'
+            component={this.renderField}
+          />
+          <Field
+            label='Post Content'
+            name='content'
+            component={this.renderField}
+          />
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
       </div>
     )
   }
 }
+
+function validate (values) {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = <h3 className='warnings'>Enter a Title</h3>
+  }
+
+  if (!values.categories) {
+    errors.categories = <h3 className='warnings'>Enter a Category</h3>
+  }
+
+  if (!values.content) {
+    errors.content = <h3 className='warnings'>Enter some Content</h3>
+  }
+
+  return errors
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(Postsnew)
